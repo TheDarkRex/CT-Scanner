be296519f8d68f6b9d0f8ff07d25eec6d6c4bc15
Jakub Biernat <k.biernat@vp.pl>
2026-03-24 08:50:43 +0100

    Add minor changes in bresenham.py

--- CT-functions/bresenham.py
+++ CT-functions/bresenham.py
@@ -1,20 +1,17 @@
-def bresenham(x1,y1,x2,y2):
-    dir_x = 1 if x1 < x2 else -1
-    dir_y = 1 if y1 < y2 else -1
-
-    dx = abs(x1-x2)
-    dy = abs(y1-y2)
+def bresenham(x0, y0, x1, y1):
+    dx = x0 - x1
+    dy = y0 - y1
 
     pixels = []
     D = 2 * dy + dx
     while True:
-        pixels.append((x1, y1))
-        if x1 == x2 and y1 == y2:
+        pixels.append((x0, y0))
+        if x0 == x1 and y0 == y1:
             return pixels
         if D > 0:
-            y1 += dir_y
+            y0 += 1
             D = D + 2 * (dy - dx)
         else:
             D = D + 2 * dy
-        x1 += dir_x
+        x1 += 1
 
